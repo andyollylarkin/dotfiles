86538e53b916106c87e08068c41c18b608b042fb
Denis Davydov <d.a.davydov@rt.ru>
2020-08-31 14:53:30 +0300

    add .ideavimrc

--- sourced/env.sh
+++ sourced/env.sh
@@ -1,14 +1,14 @@
 #!/bin/zsh
 
 export PATH=/usr/local/bin:$PATH;
-export PATH=/usr/local/Cellar/php@7.3/7.3.20/bin:$PATH;
+export PATH=/usr/local/Cellar/php@7.3/7.3.21/bin:$PATH;
 #export HOME=~
 export CELLAR="$(brew --cellar)";
 export TERM=xterm-256color;
 
 # Configure PHP path
-function set_php(){
-    print "Set path to PHP interpreter"
-    read $PHP;
-    return 0;
-}
+# function set_php(){
+#     print "Set path to PHP interpreter"
+#     read $PHP;
+#     return 0;
+# }
