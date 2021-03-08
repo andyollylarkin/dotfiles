d947dc5c33c975c2e3383457871cb580f40c90a0
 denisdavydov <denisdavydov@denisdavydovs-MacBook-Air.local>
2021-03-08 23:32:40 +0300

    Move config for install via rsync

--- sourced/export.sh
+++ sourced/export.sh
@@ -2,7 +2,8 @@
 
 export LANG="en_US.UTF-8";
 export LC_ALL="en_US.UTF-8";
-export EDITOR="VIM"
-export PAGER="less"
-export LSCOLORS=gxfxbEaEBxxEhEhBaDaCaD
+export EDITOR="VIM";
+export PAGER="less";
+export LSCOLORS=gxfxbEaEBxxEhEhBaDaCaD;
+export DOTFILES=$HOME/dotfiles;
 
